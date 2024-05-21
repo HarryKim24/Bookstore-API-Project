925968c86c52bd9223d48cf40e3dcbdb6b7f2b8d
HarryKim24 <tl9434@naver.com>
2024-05-21 19:21:25 +0900

    Feat: add users api

--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const conn = require('../mariadb');
-const {StatusCodes} = require('http-status-codes');
+const {join, login, passwordResetRequest, passwordReset} = require('../controller/UserController');
 
 router.use(express.json());
 
 // 회원가입
-router.post('/join', (req, res) => {
-  const {email, password} = req.body;
-
-  let sql = 'INSERT INTO users (email, password) VALUES (?, ?)';
-  let values = [email, password];
-
-  conn.query(sql, values,
-    (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      return res.status(StatusCodes.CREATED).json(results);
-    }
-  );
-});
+router.post('/join', join);
 
 // 로그인
-router.post('/login', (req, res) => {
-  res.json('login');
-});
+router.post('/login', login);
 
 // 비밀번호 초기화 요청
-router.post('/reset', (req, res) => {
-  res.json('require reset');
-});
+router.post('/reset', passwordResetRequest);
 
 // 비밀번호 초기화
-router.put('/reset', (req, res) => {
-  res.json('reset');
-});
+router.put('/reset', passwordReset);
 
 module.exports = router
