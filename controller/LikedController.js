b338108dfcc7d5d4fa74de790d64a1d213a564ec
HarryKim24 <tl9434@naver.com>
2024-05-26 15:32:22 +0900

    Feat: add deleteLikes api

--- controller/LikedController.js
+++ controller/LikedController.js
@@ -21,7 +21,22 @@ const addLikes = (req, res) => {
 }
 
 const deleteLikes = (req, res) => {
-  res.json('좋아요 삭제');
+  const {id} = req.params;
+  const {user_id} = req.body;
+
+  let sql = 'DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?';
+  let values = [user_id, id];
+
+  conn.query(sql, values,
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+
+      return res.status(StatusCodes.OK).json(results);
+    }
+  );
 }
 
 module.exports = {
