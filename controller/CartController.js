0964f440d3da60090828eee93a7ec66fb2f909f5
HarryKim24 <tl9434@naver.com>
2024-05-27 14:11:00 +0900

    Feat: update viewCartItems api

--- controller/CartController.js
+++ controller/CartController.js
@@ -20,14 +20,15 @@ const addCartItem = (req, res) => {
 }
 
 const viewCartItems = (req, res) => {
-  const {user_id} = req.body;
+  const {user_id, selected} = req.body;
 
   let sql = `SELECT cartItems.id, book_id, title, summary, count, price 
             FROM cartItems LEFT JOIN books 
             ON cartItems.book_id = books.id
-            WHERE user_id=?;`;
+            WHERE user_id=? AND cartItems.id IN (?)`;
+  let values = [user_id, selected];
 
-  conn.query(sql, user_id,
+  conn.query(sql, values,
     (err, results) => {
       if (err) {
         console.log(err);
--- routes/carts.js
+++ routes/carts.js
@@ -7,15 +7,10 @@ router.use(express.json());
 // 장바구니 담기
 router.post('/', addCartItem);
 
-// 장바구니 조회
+// 장바구니 조회 / 선택된 장바구니 아이템 목록 조회
 router.get('/', viewCartItems);
 
 // 장바구니 도서 삭제
 router.delete('/:id', removeCartItem);
-
-// 장바구니 선택한 상품 목록 조회
-// router.get('/', (req, res) => {
-//   res.json('장바구니 선택한 상품 목록 조회');
-// });
 
 module.exports = router
